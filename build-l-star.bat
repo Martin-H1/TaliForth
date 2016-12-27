8da2deed46b1dd1a08835fd01923e9d69e5941ab
mheermance@gmail.com <mheermance@gmail.com>
2016-12-27 11:37:20 -0500

    Updated build system to produce the SBC 2.7 and l-star binaries.

--- build-l-star.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Ophis\ophis" --65c02 Tali-Main-L-Star.asm
-"C:\Users\Martin\github\BinToMon\Release\BinToMon.exe" ophis.bin > tali-l-star.txt
--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
 "C:\Program Files (x86)\Ophis\ophis" --65c02 Tali-main-B001.asm 
+mv ophis.bin tali-sbc.bin
+"C:\Program Files (x86)\Ophis\ophis" --65c02 Tali-Main-L-Star.asm
+"C:\Users\Martin\github\BinToMon\Release\BinToMon.exe" ophis.bin > tali-l-star.txt
+mv ophis.bin tali-l-star.bin
