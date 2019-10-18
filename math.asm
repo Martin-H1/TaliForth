5a40c979107811fa3f41434b1e4c8b14e3d89133
Martin-H1 <mheermance@gmail.com>
2019-10-18 09:44:40 -0400

    added 16 bit invert and negate.

--- math.asm
+++ math.asm
@@ -8,6 +8,27 @@
 	`drop
 .macend
 
+.macro invert
+	lda #$FF
+	eor TOS_LSB,x
+	sta TOS_LSB,x
+	lda #$FF
+	eor TOS_MSB,x
+	sta TOS_MSB,x
+.macend
+
+.macro negate
+	lda TOS_LSB,x
+	eor #$FF        ; invert and add one
+	clc
+	adc #$01
+	sta TOS_LSB,x
+	lda TOS_MSB,x
+	eor #$FF       	; invert and add any carry
+	adc #$00
+	sta TOS_MSB,x
+.macend
+
 .macro orw
 	lda TOS_LSB,x
 	ora NOS_LSB,x
